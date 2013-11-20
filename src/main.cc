2bbc9195827c596ede398071d9f1a75813281175
wanyer <xiaowanyer@126.com>
2013-11-20 21:39:53 +0800

    optimize code structure

--- src/main.cc
+++ src/main.cc
@@ -8,13 +8,12 @@
 
 struct Stat
 {
-	Stat() : valid(false),
+	Stat() :
 		total_time(0),
 		hi_speed(999999),
 		lo_speed(0),
 		num(0) {}
 	std::string training_str;
-	bool valid;
 	double total_time;
 	double hi_speed;
 	double lo_speed;
@@ -23,8 +22,6 @@ struct Stat
 
 void PrintStat(const Stat& stat)
 {
-	if (!stat.valid)
-		return;
 	printf("\nresult :\n");
 	printf("\ttraining string: %s\n", stat.training_str.c_str());
 	printf("\ttrain num:  %6d\n", stat.num);
@@ -37,50 +34,27 @@ void PrintStat(const Stat& stat)
 		stat.lo_speed, stat.training_str.length() / stat.lo_speed);
 }
 
-Stat Train() 
+Stat Train(const std::string& training_str) 
 {
 	Stat ret_stat;
-	std::string training_str;
-	std::cout << "training string (\"quit\" to exit):" << std::endl;
-	std::cin >> training_str;
-	if (training_str == "quit") 
-	{
-		std::cout << "training finish!" << std::endl;
-		return ret_stat;
-	} 
+	
 	ret_stat.training_str = training_str;
 	std::cout << "start!" << std::endl;
 
 	while (1) 
 	{
 		char c;
-		while (c = _getch()) 
-		{
-			if (c == training_str[0])
+		clock_t s_time;
+		for (unsigned int i = 0; i < training_str.length(); ++i) {
+			while (c = _getch())
 			{
-				std::cout << c;
-				break;
-			}
-			else if (c == 27) // ESC
-			{
-				return ret_stat;
-			}
-		}
-
-		clock_t s_time = clock();
-		for (unsigned int i = 1; i < training_str.length(); ++i) {
-			while (1) 
-			{
-				c = _getch();
 				if (c == training_str[i])
-				{
 					break;
-				}
 				else if (c == 27) // ESC
-				{
 					return ret_stat;
-				}
 			}
+			if (i == 0)
+				s_time = clock();
 			std::cout << c;
 		}
 		double time = (double)(clock() - s_time) / CLOCKS_PER_SEC;
@@ -90,16 +64,22 @@ Stat Train()
 			ret_stat.lo_speed = time;
 		if (time < ret_stat.hi_speed)
 			ret_stat.hi_speed = time;
-		ret_stat.valid = true;
 		printf("\ncost: %6.3f s\n", time);
 	}
 }
 
 int main() {
 	while (1) {
-		Stat stat = Train();
-		if (!stat.valid)
+		std::string training_str;
+		std::cout << "training string (\"quit\" to exit):" << std::endl;
+		std::cin >> training_str;
+		if (training_str == "quit")
+		{
+			std::cout << "training finish!" << std::endl;
 			break;
+		} 	
+		
+		Stat stat = Train(training_str);
 		PrintStat(stat);
 	}
 	return 0;
